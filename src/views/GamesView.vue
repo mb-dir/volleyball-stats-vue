b3534225cdf2ac35b406b4d9a974b3caacaa902b
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-10-30 17:35:52 +0100

    Create basic view for routes

---
+++ src/views/GamesView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="mainContainer">
+    <h2>Mecze to będą pokazane xdxdxd</h2>
+  </main>
+</template>
+
+<style scoped>
+.mainContainer {
+  text-align: center;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+}
+</style>
---
+++ src/views/TableView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="mainContainer">
+    <h2>Tabela to będzie kozacka</h2>
+  </main>
+</template>
+
+<style scoped>
+.mainContainer {
+  text-align: center;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+}
+</style>
