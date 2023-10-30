862f8818e4750669cc87a44b90b46f61df57b451
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-10-30 17:36:03 +0100

    Update routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import HomeView from '../views/HomeView.vue'
+import GamesView from "../views/GamesView.vue";
+import HomeView from "../views/HomeView.vue";
+import TableView from "../views/TableView.vue";
+import TeamsView from "../views/TeamsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
-  ]
-})
+    {
+      path: "/tabela",
+      name: "table",
+      component: TableView,
+    },
+    {
+      path: "/mecze",
+      name: "games",
+      component: GamesView,
+    },
+    {
+      path: "/kluby",
+      name: "teams",
+      component: TeamsView,
+    },
+  ],
+});
 
-export default router
+export default router;
