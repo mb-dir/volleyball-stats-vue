3e036337cac199e870d8917aeedbf544293594a1
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-10-30 22:32:09 +0100

    Fetch data

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -1,4 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { singleGame, gamesResponse } from "@/types";
+import { ref, type Ref } from "vue";
+import SortedTable from "@/components/SortedTable.vue";
+
+const games: Ref<singleGame[]> = ref([]);
+
+const getData = async () => {
+  try {
+    const res = await fetch(
+      "https://api-volleyball.p.rapidapi.com/games?league=113&season=2023",
+      {
+        headers: {
+          "x-rapidapi-key":
+            "df10653d8bmsh4483ed61b853b26p135ae4jsn28f7cc6f2c6e",
+        },
+      }
+    );
+    const data: gamesResponse = await res.json();
+    games.value = data.response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
+</script>
 
 <template>
   <main class="mainContainer">
