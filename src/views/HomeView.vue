76d31f980fbe73412387546a46aae0c23d47d8ab
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-10-30 17:32:23 +0100

    Content for home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>Home</main>
+  <main class="mainContainer">
+    <h2>Volley-Stats</h2>
+    <p>Wszystkie najważniejsze dane dotyczące PlusLigi w jednym miejscu</p>
+  </main>
 </template>
+
+<style scoped>
+.mainContainer {
+  text-align: center;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+}
+</style>
